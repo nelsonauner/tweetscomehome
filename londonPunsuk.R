56b68fe2752fe00f3c49ff9b7e89c06d67622622
Nelson Auner <you@example.com>
2014-11-23 17:12:13 -0600

    updated analysis with geographic maps

--- londonPunsuk.R
+++ londonPunsuk.R
@@ -10,7 +10,7 @@ bigd <- read.csv('~/Home/LondonData/london2012-tweets/takeOut.csv',
                  stringsAsFactors=FALSE)
 
 
-smalld <- bigd %>% head(50)
+reald <- bigd %>% head(50)
 # Easy to play around with this. 
 
 bigd$longitude <-bigd$tweet_longitude %>% as.character %>% as.numeric 
@@ -29,14 +29,74 @@ reald <- bigd %>% filter(tweet_longitude %>% ftn %>% is.na ==FALSE,
 library(tm);library(textir);library(SnowballC)
 
 
-# Map all postal codes, with code that can be rerun upon error 
+# Map all postal codes, with code that can be rerun upon error.  
 reald$pdistrict <- NA
-for (j in reald$pdistrict %>% is.na %>% which) {
+for (i in 1:dim(reald)[1]) {
+  print(i)
   reald$pdistrict[i] <- findPostalCode(reald$tweet_longitude[i],reald$tweet_latitude[i],ct)
 }
 
-# And combine into a unique postalcode - date (year?)
+# Use realds as smaller one. 
+ realds$latlong <- paste(realds$tweet_latitude,realds$tweet_longitude,sep=":")
 
+
+# sample geo graph: 
+
+
+AndrewMap <- gvisMap(realds %>% head(100), "latlong" , "tweet_text", 
+                     options=list(showTip=TRUE, 
+                                  showLine=TRUE, 
+                                  enableScrollWheel=TRUE,
+                                  mapType='terrain', 
+                                  useMapTypeControl=TRUE))
+
+
+l# And combine into a unique postalcode - date (year?)
+tsum <- 
+reald %>% group_by(pdistrict) %>% 
+  summarise(ntweets = n(),
+            users = length(unique(user_id)),
+            friends = user_friends_count %>% as.numeric %>% sum,
+            followers = user_followers_count %>% as.numeric %>% sum,
+            fromiPhone = sum(tweet_source == "Twitter for iPhone"),
+            fromAndroid = sum(tweet_source == "Twitter for Android"),
+            fromiPad = sum(tweet_source == "Twitter for iPad"),
+            fromBlackBerry = sum(tweet_source == "Twitter for BlackBerry®"),
+            fromInsta = sum(tweet_source == "Instagram"),
+            content = tweet_text %>% paste(collapse= "|")
+            
+# This works perfectly. Now add back latlong using pdistrict
+tsum$latlong = sapply(tsum$pdistrict,FUN= function(pc) {
+  ct[ct$pdisrict == pc %>% as.character,][c("latitude","longitude")] %>% paste(collapse=":")
+})
+
+
+#That worked well too. Now, let's wrap it into a graph: 
+PostTweets <- gvisGeoChart(tsum,
+                           locationvar="latlong",
+                           colorvar = "users",
+                           sizevar="ntweets",
+                           options=list(
+                             height=700,
+                             #region="GB",
+                             displayMode='Markers',
+                             colorAxis="{colors:['purple', 'red', 'orange','gray']}",
+                             backgroundColor = "lightblue"))
+                             chartid = "EQ")
+)
+)
+                             #displayMode="regions",
+                             #resolution="provinces",
+                             #width=600,
+                             #height=400))
+
+plot(PostTweets)
+tsum <- cbind(tsum, do.call(rbind,
+                            tsum$latlong %>% strsplit(":")))
+names(tsum)[13:14] <- c("x","y")
+
+map <-  get_googlemap('london',markers = tsum[c("x","y")]) 
+ggmap(map) + geom_point(x=)
 
 tweetCorpus <- reald$tweet_text %>% as.character %>% VectorSource %>% Corpus
 
@@ -72,8 +132,8 @@ cl <- makeCluster(detectCores(), type="FORK")
 fitTweets <- mnlm(cl = cl,
                   covars = reald$tweet_retweet_count %>% ftn,
                   counts = TDM)
-                  
-                  
+
+
 stopCluster(cl)
 
 
